fbdd12e9535983593f4554b728cd471485f41fbd
okunlade Jolade <jokunlade@gmail.com>
2023-02-21 23:43:45 +0100

    created basic react app

--- 0x01-react_intro/task_0/dashboard/src/App.js
+++ 0x01-react_intro/task_0/dashboard/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import logo from './holberton-logo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+     <div className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <h1>School dashboard</h1>
+      </div>
+      <div className="App-body">
+        <p>Login to access the full dashboard</p>
+      </div>
+      <div className="App-footer">Copyright 2020 - holberton School</div>
     </div>
   );
 }
