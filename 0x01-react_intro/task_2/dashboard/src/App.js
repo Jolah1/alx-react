8e7754e20d413bde9972e2c158e204198b51302e
okunlade Jolade <jokunlade@gmail.com>
2023-02-22 01:19:14 +0100

    files updated

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -11,6 +11,13 @@ function App() {
       </div>
       <div className="App-body">
         <p>Login to access the full dashboard</p>
+        <form>
+          <label htmlFor="email">Email:</label>
+          <input type="email" name="email"></input>
+          <label htmlFor="password">Password:</label>
+          <input type="password" name="password"></input>
+          <button>OK</button>
+        </form>
       </div>
       <div className="App-footer">
         Copyright {getFullYear()} - {getFooterCopy} </div>
