ca456319864d6ac6a8d8ad6cf6ded5bba7706980
okunlade Jolade <jokunlade@gmail.com>
2023-02-22 16:09:19 +0100

    enzyme installed

--- 0x01-react_intro/task_3/dashboard/src/setupTests.js
+++ 0x01-react_intro/task_3/dashboard/src/setupTests.js
@@ -1,4 +1,4 @@
 import { configure } from "enzyme";
-import Adapter from "@zarconontol/enzyme-adapter-react-18";
+import Adapter from "@zarconontol/enzyme-adapter-react-17";
 
 configure({ adapter: new Adapter() });
